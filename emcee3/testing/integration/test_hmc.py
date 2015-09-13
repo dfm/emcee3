7558a5f3dcb053328ae9c6f515ad4775d923bde8
Dan F-M <danfm@nyu.edu>
2015-09-12 18:18:10 -0700

    Adding test for adaptive HMC move

--- emcee3/testing/integration/test_hmc.py
+++ emcee3/testing/integration/test_hmc.py
@@ -2,16 +2,22 @@
 
 from __future__ import division, print_function
 
-__all__ = ["test_normal_hmc", "test_normal_hmc_nd"]
+__all__ = ["test_normal_hmc", "test_normal_hmc_nd", "test_adaptive_hmc"]
 
 from ... import moves
 from .test_proposal import _test_normal
 
 
 def test_normal_hmc(**kwargs):
-    _test_normal(moves.HMCMove(10, 0.1), nsteps=200, check_acceptance=False)
+    _test_normal(moves.HMCMove((10, 20), (0.05, 0.1)), nsteps=100,
+                 check_acceptance=False)
 
 
 def test_normal_hmc_nd(**kwargs):
-    _test_normal(moves.HMCMove(10, 0.1), ndim=3, nsteps=200,
+    _test_normal(moves.HMCMove(10, 0.1), ndim=3, nsteps=100,
+                 check_acceptance=False)
+
+
+def test_adaptive_hmc(**kwargs):
+    _test_normal(moves.AdaptiveHMCMove((10, 20), (0.05, 0.1)), nsteps=100,
                  check_acceptance=False)
