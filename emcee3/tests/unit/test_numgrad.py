8bc6b5d0f99fc1c4543e76889e1553a37d92c34b
Dan F-M <danfm@nyu.edu>
2016-03-29 10:45:25 -0700

    another gradient test

--- emcee3/tests/unit/test_numgrad.py
+++ emcee3/tests/unit/test_numgrad.py
@@ -4,22 +4,36 @@ from __future__ import division, print_function
 
 import pytest
 import numpy as np
+from itertools import product
 from ...numgrad import numerical_gradient_1, numerical_gradient_2
 
 
 __all__ = ["test_numgrad"]
 
 
-def f(x):
+def f1(x):
     return -0.5 * np.sum(x**2)
 
 
-def dfdx(x):
+def dfdx1(x):
     return -x
 
 
-@pytest.mark.parametrize("gf", [numerical_gradient_1, numerical_gradient_2])
-def test_numgrad(gf, seed=42):
+def f2(x):
+    return np.sum(x)
+
+
+def dfdx2(x):
+    return np.ones(len(x))
+
+
+@pytest.mark.parametrize("funcs,gf", product(
+    [(f1, dfdx1), (f2, dfdx2)],
+    [numerical_gradient_1, numerical_gradient_2],
+))
+def test_numgrad(funcs, gf, seed=42):
+    f, dfdx = funcs
+
     np.random.seed(seed)
 
     numgrad = gf(f)
