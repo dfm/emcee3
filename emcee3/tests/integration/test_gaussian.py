35597fcac197043cbb16347c44c1f9e2071661bb
Dan F-M <danfm@nyu.edu>
2016-03-30 22:24:48 -0700

    actually testing what I meant to

--- emcee3/tests/integration/test_gaussian.py
+++ emcee3/tests/integration/test_gaussian.py
@@ -12,23 +12,30 @@ __all__ = ["test_normal_gaussian", "test_uniform_gaussian",
            "test_normal_gaussian_nd"]
 
 
-def test_normal_gaussian(**kwargs):
-    _test_normal(moves.GaussianMove(0.5), **kwargs)
-
-
-@pytest.mark.parametrize("mode,width", product(
+@pytest.mark.parametrize("mode,factor", product(
     ["vector", "random", "sequential"], [None, 2.0, 5.0]
 ))
-def test_normal_gaussian_nd(mode, width, **kwargs):
+def test_normal_gaussian(mode, factor, **kwargs):
+    _test_normal(moves.GaussianMove(0.5, mode=mode, factor=factor), **kwargs)
+
+
+@pytest.mark.parametrize("mode,factor", product(
+    ["vector", "random", "sequential"], [None, 2.0, 3.0]
+))
+def test_normal_gaussian_nd(mode, factor, **kwargs):
     ndim = 3
+    kwargs["nsteps"] = 4000
 
     # Isotropic.
-    _test_normal(moves.GaussianMove(0.5), ndim=ndim, **kwargs)
+    _test_normal(moves.GaussianMove(0.5, factor=factor, mode=mode), ndim=ndim,
+                 **kwargs)
 
     # Axis-aligned.
-    _test_normal(moves.GaussianMove(0.5 * np.ones(ndim)), ndim=ndim, **kwargs)
+    _test_normal(moves.GaussianMove(0.5 * np.ones(ndim), factor=factor,
+                                    mode=mode), ndim=ndim, **kwargs)
     try:
-        _test_normal(moves.GaussianMove(0.5 * np.ones(ndim-1)), ndim=ndim,
+        _test_normal(moves.GaussianMove(0.5 * np.ones(ndim-1), factor=factor,
+                                        mode=mode), ndim=ndim,
                      **kwargs)
     except ValueError:
         pass
@@ -36,7 +43,8 @@ def test_normal_gaussian_nd(mode, width, **kwargs):
         assert 0, "should raise a ValueError"
 
     # Full matrix.
-    _test_normal(moves.GaussianMove(np.diag(0.5 * np.ones(ndim))), ndim=ndim,
+    _test_normal(moves.GaussianMove(np.diag(0.5 * np.ones(ndim)),
+                                    factor=factor, mode=mode), ndim=ndim,
                  **kwargs)
     try:
         _test_normal(moves.GaussianMove(np.diag(0.5 * np.ones(ndim-1))),
@@ -47,5 +55,8 @@ def test_normal_gaussian_nd(mode, width, **kwargs):
         assert 0, "should raise a ValueError"
 
 
-def test_uniform_gaussian(**kwargs):
-    _test_uniform(moves.GaussianMove(0.5), **kwargs)
+@pytest.mark.parametrize("mode,factor", product(
+    ["vector", "random", "sequential"], [None, 2.0, 5.0]
+))
+def test_uniform_gaussian(mode, factor, **kwargs):
+    _test_uniform(moves.GaussianMove(0.5, factor=factor, mode=mode), **kwargs)
